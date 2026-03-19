d1af5eb6d033a038e7ed87e15cee64050e503a57
edwardvanechoud <e24660@eon.com>
2026-03-19 21:52:46 +0100

    added urls

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
+  site: 'https://edwardvaneechoud.github.io',
+  base: '/pyfloe-tutorial',
   output: 'static',
   build: {
     assets: '_assets',
